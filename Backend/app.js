62f6ef909de8f356b3e5cc56f51905ef4b7ca6c5
AishwaryaBaniya <aishwarya59bainya@gmail.com>
2024-08-07 21:24:32 +0545

    Saved Changes

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,4 @@
 require('dotenv').config()
-
 const express = require('express')
 const expressLayout = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
@@ -7,38 +6,53 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
-const bcrypt = require('bcrypt')
+const bcryptjs = require('bcryptjs')
 const cookieParser = require('cookie-parser')
 const jwt = require('jsonwebtoken')
 const methodOverride = require('method-override')
+const multer = require('multer')
+const path = require('path')
+const authRoutes = require('./routes/auth')
+const isAdmin = require('./middleware/auth')
+
 
 const app = express()
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000;
+
 
 //Middleware
-app.use(expressLayout)
-app.set('layout', './layouts/main')
+	@@ -29,6 +30,12 @@ app.use(cors())
+app.use(cookieParser())
+app.use(express.json())
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+//set view Engine
 app.set('view engine', 'ejs')
 
-app.use(bodyParser.json())
-app.use(cors())
-app.use(cookieParser())
-
-//Routes
-app.use('/', require('./server/routes/main'))
+	@@ -43,13 +50,16 @@ app.use('/', indexRouter)
+app.use('/', authRoutes)
+//app.use('/books', booksRouter)
+app.use('/cart', cartRouter)
+app.use('/', adminRouter)
 
 //Port
 app.listen(port, () => {
     console.log(`Server running on port http://localhost:${port}/`)
 })
 
+
+const secretKey = process.env.SECRET_KEY || 'default_secret_key'; // Fallback for development
+
 //connect to MongoDB
-// mongoose.connect('mongodb+srv://anuusapkota10:ow7d3ZyV6CpN0SHe@cluster0.3m1dv67.mongodb.net/PustakPanna?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect('mongodb+srv://anuusapkota10:ow7d3ZyV6CpN0SHe@cluster0.3m1dv67.mongodb.net/PustakPanna?retryWrites=true&w=majority&appName=Cluster0').then(() => console.log('MongoDB connected')).catch(err => console.log('MongoDB connection error:', err));
 
-// const db = mongoose.connection
-// db.on('error', console.error.bind(console, 'MongoDB connection error: '))
+	@@ -69,5 +79,5 @@ const storage = multer.diskStorage({
+    }
+  });
 
-//public
-app.use(express.static('public'))
-
-
+  app.use('/images', express.static(path.join(__dirname, 'public/images')));
