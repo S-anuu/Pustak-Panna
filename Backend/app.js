c941850d8ae04da8364bee0f02f4230bd1fc078b
sauravdhakal12 <sauravdhakal121@gmail.com>
2024-08-15 20:40:42 +0545

    Testing vercel deploy

--- Backend/app.js
+++ Backend/app.js
@@ -17,7 +17,7 @@ const User = require('./models/User')
 const jwt = require('jsonwebtoken')
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // Session configuration
 app.use(session({
